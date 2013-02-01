9c6dec1ce1382463a21623706815a59166a466ff
David Beckley <beckl.d.413@isg.edu.sa>
2013-02-01 22:00:56 +0300

    Added wrapper for disassemble

--- src/jupiter.cpp
+++ src/jupiter.cpp
@@ -34,6 +34,7 @@ extern "C"
 
 static PyObject *JupiterError;
 
+// TODO: accept both a string or a sequence of strings
 static PyObject * jupiter_assemble(PyObject *self, PyObject *args)
 {
     const char *assembly_code;
@@ -58,6 +59,9 @@ static PyObject * jupiter_assemble(PyObject *self, PyObject *args)
     int length = object_code.size();
 
     PyObject * bytes = PyList_New(length);
+    if(bytes == NULL) {
+        return NULL;
+    }
 
     for(int i = 0; i < length; i++) {
         if(PyList_SetItem(bytes, i, PyLong_FromLong(object_code[i])) < 0) {
@@ -69,9 +73,76 @@ static PyObject * jupiter_assemble(PyObject *self, PyObject *args)
     return bytes;
 }
 
+static PyObject * jupiter_disassemble(PyObject *self, PyObject *args)
+{
+    PyObject * bytes = PyTuple_GetItem(args, 0);
+
+    if(bytes == NULL) {
+        return NULL;
+    }
+
+    if(PySequence_Check(bytes) != 1) {
+        PyErr_SetString(PyExc_TypeError, "Non-sequence argument");
+        return NULL;
+    }
+
+    std::vector<std::uint16_t> object_code;
+    
+    int length = PySequence_Size(bytes);
+    if(length < 0) {
+        return NULL;
+    }
+
+    for(int i = 0; i < length; i++) {
+        PyObject * byte = PySequence_GetItem(bytes, i);
+        if(byte == NULL) {
+            return NULL;
+        }
+        
+        if(PyNumber_Check(byte) != 1) {
+            Py_DECREF(byte);
+            PyErr_SetString(PyExc_TypeError, "Non-numeric value in sequence");
+            return NULL;
+        }
+        
+        object_code.push_back(PyLong_AsLong(byte));
+	Py_DECREF(byte);
+    }
+    
+    std::vector<std::string> lines;
+    lines = galaxy::jupiter::disassemble(object_code.begin(),
+                                         object_code.end());
+    
+    length = lines.size();
+
+    PyObject * lines_list = PyList_New(length);
+    if(lines_list == NULL) {
+        return NULL;
+    }
+
+    for(int i = 0; i < length; i++) {
+        PyObject * line = PyUnicode_FromString(lines[i].c_str());
+        if(line == NULL) {
+            Py_DECREF(lines_list);
+            Py_DECREF(line);
+            return NULL;
+        }
+
+        if(PyList_SetItem(lines_list, i, line) < 0) {
+            Py_DECREF(lines_list);
+            Py_DECREF(line);
+            return NULL;
+        }
+    }
+
+    return lines_list;
+}
+
 static PyMethodDef JupiterMethods[] = {
     {"assemble", jupiter_assemble, METH_VARARGS,
      "Assemble the given code into DCPU-16 machine language."},
+    {"disassemble", jupiter_disassemble, METH_VARARGS,
+     "Disassemble the given machine code into assembly language."},
     {NULL, NULL, 0, NULL}        // Sentinel
 };
 
