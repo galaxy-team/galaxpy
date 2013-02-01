103bba08a1293062c00783f5ee8d66783925cc4f
David Beckley <beckl.d.413@isg.edu.sa>
2013-02-01 09:05:09 +0300

    Reorganized

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup, Extension
 
 jupiter = Extension('jupiter',
-                    include_dirs = ['jupiter/src/lib'],
-                    sources = ['jupiter.cpp', 'jupiter/src/lib/libjupiter.cpp'])
+                    include_dirs = ['lib/jupiter/src/lib'],
+                    sources = ['src/jupiter.cpp'])
 
 jupiter.extra_compile_args = ['--std=c++11']
 
-setup (name = 'GalaxPy',
-       version = '1.0',
+setup (name = 'galaxpy',
+       version = '0.1',
+       license = 'GPL',
        description = 'A Python API to the Galaxy DCPU-16 toolchain',
+       ext_package='galaxpy',
        ext_modules = [jupiter])
