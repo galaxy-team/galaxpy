04e9ebd452aa32fe440ad6c9189282b97fac6b4d
Miles Rout <miles.rout@gmail.com>
2013-02-02 05:46:08 +1300

    Fixed licensing and naming in src/jupiter.cpp.

--- src/jupiter.cpp
+++ src/jupiter.cpp
@@ -1,22 +1,22 @@
 /*
 
-This file is part of jupiter.
+This file is part of galaxpy.
 
-jupiter is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+galaxpy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-jupiter is distributed in the hope that it will be useful,
+galaxpy is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with jupiter.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with galaxpy.  If not, see <http://www.gnu.org/licenses/>.
 
 Your copy of the GNU General Public License should be in the
-file named "LICENSE.txt".
+file named "LICENSE-LGPL.txt".
 
 */
 
