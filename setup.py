12fd8b2a7497ca304d6b339287ce141d333e29f4
David Beckley <beckl.d.413@isg.edu.sa>
2013-02-03 11:21:19 +0300

    Added stuff for sdist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 jupiter = Extension('jupiter',
                     include_dirs = ['lib/jupiter/src/lib'],
-                    sources = ['src/jupiter.cpp'])
+                    sources = ['src/jupiter.cpp', 'lib/jupiter/src/lib/libjupiter.cpp'])
 
 jupiter.extra_compile_args = ['--std=c++11']
 
@@ -10,5 +10,8 @@ setup (name = 'galaxpy',
        version = '0.1',
        license = 'GPL',
        description = 'A Python API to the Galaxy DCPU-16 toolchain',
+       maintainer = 'David Beckley',
+       maintainer_email = 'beckl.d.413@isg.edu.sa',
+       url = 'http://www.github.com/galaxy-team/galaxpy',
        ext_package='galaxpy',
        ext_modules = [jupiter])
