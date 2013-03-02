8cf6700ac23834bf3060812af8ff08529d86df51
David Beckley <beckl.d.413@isg.edu.sa>
2013-03-02 21:12:03 +0300

    Add directory to the include path

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
 jupiter = Extension('jupiter',
-                    include_dirs = ['lib/jupiter/src/lib'],
+                    include_dirs = ['lib/jupiter/src/lib',
+                                    'lib/jupiter/lib/asteroid'],
                     sources = ['src/jupiter.cpp', 'lib/jupiter/src/lib/libjupiter.cpp'])
 
 jupiter.extra_compile_args = ['--std=c++11']
