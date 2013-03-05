f98c2213d19bea532d7c2f177b32dbba150eac12
Dominic May <jack.thatch@gmail.com>
2013-03-05 20:31:55 +0800

    Fixed tests :P (and ye godz, the dev world is pushing travis-ci to its limits tonight :/)

--- tests/test_saturn.py
+++ tests/test_saturn.py
@@ -12,18 +12,17 @@ class Test_Saturn(unittest.TestCase):
 
 #        self.assertEqual()
 
-        self.reset()
+        self.cpu.reset()
     
     def test_flash(self):
         opcodes = [0x7c01, 0x0f00, 0x7fc1, 0x0001, 0xdead, 0x03c2, 0xdead]
 
         self.cpu.flash(opcodes)
 
-        self.assertTrue(list(cpu).startswith(opcodes))
+        self.assertTrue(list(self.cpu)[:len(opcodes)] == opcodes)
 
-        self.reset()
+        self.cpu.reset()
         
-
 
 def main():
     unittest2.main()
